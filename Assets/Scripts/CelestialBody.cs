bc40ea5e0c9181e3a2ee8099836075db6f7f302e
Thesupernile <thesupernile6@gmail.com>
2025-06-02 10:11:37 +0100

    Started work on implementing gravity

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace SolarSystemResources
+{
+    public struct CelestialBody
+    {
+        GameObject body;
+        Vector3 position;
+        Vector3 startingVelocity;
+        Vector3 scale;
+    }
+}
--- Assets/Scripts/SolarSystem.cs
+++ Assets/Scripts/SolarSystem.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using System;
 
 public class SolarSystem : MonoBehaviour
 {
+    // Temp Variable for testing
     public GameObject celestialBodyTemplate;
+    // Main Variables
+    public SolarSystemResources.CelestialBody[] celestialBodies;
+    public const float GravitationalConstant = (6.67 * Math.Pow(10, -11));
     GameObject sun;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -13,12 +18,21 @@ public class SolarSystem : MonoBehaviour
         // Creation of the sun
         sun = Instantiate(celestialBodyTemplate);
         sun.SetActive(true);
+        sun.transform.localScale = new Vector3(100, 100, 100);
+        sun.transform.position = new Vector3(0, 0, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         // Using the universal law of gravitation, attract each of the celestial bodies to each of the other bodies
+        foreach (SolarSystemResources.CelestialBody targetCelestialBody in celestialBodies) {
+            foreach (SolarSystemResources.CelestialBody secondaryCelestialBody in celestialBodies) {
+                
+            }
+        }
+        float mass = sun.GetComponent<Rigidbody>().mass;
 
     }
 }
